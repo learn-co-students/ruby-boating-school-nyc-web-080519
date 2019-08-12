3be27fce8fff9b286cb29ea0d9745389f421d4c1
DanBehrman <dan.behrman@wework.com>
2019-08-12 00:25:15 -0400

    Done.

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,40 @@
 class Instructor
+    attr_reader :name
 
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def get_matching_test(student, test_name)
+        BoatingTest.all.select do |boatingTest|
+            boatingTest.student == student && boatingTest.name == test_name
+        end
+    end
+
+    def pass_student(student, test_name)
+        my_test = get_matching_test(student, test_name)
+        if my_test.length > 0
+            my_test[0].status = "passed"
+            return my_test[0]
+        else
+            return BoatingTest.new(student, test_name, "passed", self)
+        end
+    end
+
+    def fail_student(student, test_name)
+        my_test = get_matching_test(student, test_name)
+        if my_test.length > 0
+            my_test[0].status = "failed"
+            return my_test[0]
+        else
+            return BoatingTest.new(student, test_name, "failed", self)
+        end
+    end
+
+    def self.all
+        @@all
+    end
 end
